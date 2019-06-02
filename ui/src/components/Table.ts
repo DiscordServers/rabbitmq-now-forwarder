c026d95fdb125d92160a546cd205141bba74598f
Arunesh <arunesh90@outlook.com>
2019-06-02 06:24:03 +0200

    Added Table & Improved instance page

---
+++ ui/src/components/Table.ts
@@ -0,0 +1,53 @@
+// From https://github.com/zeit/integrations/blob/master/easycron/src/components/Field.ts
+import {htm as html} from '@zeit/integration-utils';
+
+export const Table = ({header, children}: {header: any; children: any}) => html`
+    <Box
+        width="100%"
+        display="table"
+        borderRadius="5px"
+        borderColor="#eaeaea"
+        overflow="hidden"
+        borderStyle="solid"
+        borderWidth="1px"
+        marginTop="20px"
+        marginBottom="20px"
+    >
+        <Box display="table-header-group" backgroundColor="rgb(250, 250, 250)">
+            <Box display="table-row">
+                ${header}
+            </Box>
+        </Box>
+        <Box display="table-row-group">
+            ${children}
+        </Box>
+    </Box>
+`;
+
+export const TableRow = ({children}: {children: any}) => html`
+    <Box
+        display="table-row"
+        borderColor="#eaeaea"
+        borderStyle="solid"
+        borderBottomWidth="1px"
+        backgroundColor="white"
+    >
+        ${children}
+    </Box>
+`;
+
+export const HeaderItem = ({children}: {children: any}) => html`
+    <Box
+        display="table-cell"
+        padding="10px"
+        borderColor="#eaeaea"
+        borderStyle="solid"
+        borderWidth="0px"
+        borderBottomWidth="1px"
+        ><p><b>${children}</b></p></Box
+    >
+`;
+
+export const BodyItem = ({children}: {children: any}) => html`
+    <Box display="table-cell" padding="10px"><p>${children}</p></Box>
+`;
--- ui/src/pages/viewInstance.ts
+++ ui/src/pages/viewInstance.ts
@@ -1,5 +1,6 @@
 import {htm, HandlerOptions} from '@zeit/integration-utils';
 import {getInstances, addListener, deleteListener} from '../utils/instances';
+import {Table, HeaderItem, TableRow, BodyItem} from '../components/Table';
 
 let formStore = {
     listenerEndpoint: '',
@@ -86,35 +87,42 @@ export default async function viewInstance(handler: HandlerOptions) {
                 : ''
         }
 
-        <H2>Listeners</H2>
-        <UL>
-          <Box margin-bottom="50px">
-            ${instance.listeners.map((listener, index) => {
-                return htm`
-                <Box padding-bottom="4px" word-break="break-all" width="25rem" border-bottom="1px solid black">
-                  <B>Webhook URL:</B> ${listener.endpoint}
-                  <BR />
-                  <B>Queue:</B> ${listener.queue}
-                  <BR />
-                  <Button small themeColor="red" action=${`delete-listener-${index}`}>Delete</Button>
+        <H1>Listeners</H1>
+        <${Table} header=${htm`
+          <${HeaderItem}>Endpoint</${HeaderItem}>
+          <${HeaderItem}>Queue</${HeaderItem}>
+          <${HeaderItem}>Delete Endpoint</${HeaderItem}>
+        `}>
+          ${instance.listeners.map((listener, index) => {
+              return htm`
+              <${TableRow}>
+                <${BodyItem}>${listener.endpoint}</${BodyItem}>
+                <${BodyItem}>${listener.queue}</${BodyItem}>
+                <${BodyItem}><Button small themeColor="red" action=${`delete-listener-${index}`}>Delete</Button></${BodyItem}>
+              </${TableRow}>
+            `;
+          })}
+        </${Table}>
+
+        <H1>Add listener</H1>
+        <Fieldset>
+            <FsContent>
+              <Box display="flex">
+                <Box padding-right="10px">
+                  <Input name="listenerEndpoint" label="Listener Endpoint" value=${
+                      formStore.listenerEndpoint
+                  } />
                 </Box>
-              `;
-            })}
-          </Box>
-        </UL>
-        <H2>Add listener</H2>
-        <Box>
-          <Input name="listenerEndpoint" placeholder="Listener Endpoint" value=${
-              formStore.listenerEndpoint
-          } />
-          <BR />
-          <Input name="listenerQueue" placeholder="Listener Queue" value=${
-              formStore.listenerQueue
-          } />
-          <BR />
-          <BR />
-          <Button action="submit-listener">Add Listener</Button>
-        </Box>
+                <BR />
+                <Input name="listenerQueue" label="Listener Queue" value=${
+                    formStore.listenerQueue
+                } />
+              </Box>
+            </FsContent>
+            <FsFooter>
+              <Button action="submit-listener">Add Listener</Button>
+            </FsFooter>
+        </Fieldset>
         <Box display="none">
           <Input name="instanceId" value=${instanceId} />
         </Box>
