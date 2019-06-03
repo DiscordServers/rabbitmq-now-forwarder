e62d3d8291748bf258d0aa248e23ffd2fc652e38
Aaron Scherer <aequasi@gmail.com>
2019-06-02 20:19:29 -0700

    Removing installation id

--- listener/src/util/getMetadata.ts
+++ listener/src/util/getMetadata.ts
@@ -3,6 +3,7 @@ import Configuration from '../types/Collection/Configuration';
 import Metadata from '../types/Metadata';
 
 const getMetadata = async (configuration: Configuration): Promise<Metadata> => {
+    console.log(configuration);
     const response = await fetch(
         `https://api.zeit.co/v1/integrations/configuration/${configuration.configurationId}/metadata`,
         {
