888381d56db0028a6fb9ed24705418272db3af4a
Aaron Scherer <aequasi@gmail.com>
2019-06-02 22:47:01 -0700

    Fixing url

--- ui/src/utils/instances.ts
+++ ui/src/utils/instances.ts
@@ -132,7 +132,6 @@ export async function getGeneratedKey(configurationId: string): Promise<string> 
 }
 
 export async function regenerateKey(configurationId: string, handler: HandlerOptions): Promise<string> {
-    console.log(`${process.env.ZEIT_HOOK_URL}/regenerateKey/${configurationId}`);
     const response = await fetch(`${process.env.ZEIT_HOOK_URL}/regenerateKey/${configurationId}`, {
         method: 'POST',
         headers: {
